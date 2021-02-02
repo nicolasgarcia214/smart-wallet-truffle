9a94445e70df1d8470f88d0372f3acfe7c482267
Nicolas Garcia <nicolasgarcia214@gmail.com>
2021-02-02 00:03:15 -0500

    🛠 Fixed migration files and added onlyOwner modifier

--- contracts/SimpleWallet.sol
+++ contracts/SimpleWallet.sol
@@ -1,16 +1,33 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.22 <0.8.0;
+pragma solidity >=0.8.0;
 
 contract SimpleWallet {
     event Received(address, uint);
-    
-    function withdrawMoney(address payable _to, uint _amount) public {
+    uint public totalBalance;
+    address public owner;
 
+    mapping (address=>uint) public allowance;
+
+    constructor(){
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(owner == msg.sender, "You are not the owner");
+        _
+    }
+    
+    function withdrawMoney(address payable _to, uint _amount) public onlyOwner {
+        totalBalance -= _amount;
+        _to.transfer(_amount);
     }
 
     receive() external payable {
+        totalBalance += msg.value;
         emit Received(msg.sender, msg.value);
     }
 
-    fallback () external payable {}
+    fallback () external payable {
+        totalBalance += msg.value;
+    }
 }
