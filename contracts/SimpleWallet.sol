967612aff0763011d57d3124d734ed36646bc8a2
Nicolas Garcia <nicolasgarcia214@gmail.com>
2021-02-02 00:30:24 -0500

    🎆 Allowance Init

--- contracts/SimpleWallet.sol
+++ contracts/SimpleWallet.sol
@@ -14,10 +14,19 @@ contract SimpleWallet {
 
     modifier onlyOwner() {
         require(owner == msg.sender, "You are not the owner");
-        _
+        _;
+    }
+
+    modifier ownerOrAllowed(uint _amount) {
+        require((owner == msg.sender) || (allowance[msg.sender] >= _amount), "You are not allowed");
+        _;
+    }
+
+    function setAllowance(address _who, uint _amount) public onlyOwner{
+        allowance[_who] = _amount;
     }
     
-    function withdrawMoney(address payable _to, uint _amount) public onlyOwner {
+    function withdrawMoney(address payable _to, uint _amount) public ownerOrAllowed(_amount) {
         totalBalance -= _amount;
         _to.transfer(_amount);
     }
